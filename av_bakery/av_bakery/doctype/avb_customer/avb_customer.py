2a58a6283f9e3c11fa91706b82594a6f4d9cef94
MariyaAnsi <mariya.ansi1998@gmail.com>
2022-01-31 14:40:52 +0530

    av-bakery app created

---
+++ av_bakery/av_bakery/doctype/avb_customer/avb_customer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, mariya@yuvabe.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class AVBCustomer(Document):
+	pass
---
+++ av_bakery/av_bakery/doctype/avb_customer/test_avb_customer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, mariya@yuvabe.com and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAVBCustomer(unittest.TestCase):
+	pass
